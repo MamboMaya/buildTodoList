a83b38d2f03931598a6a1f13f039068e6fc41728
Damien Maya <damien_maya@yahoo.com>
2017-06-28 16:22:33 -0500

    added SQL

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mustache = require('mustache-express')
 const bodyParser = require('body-parser')
 const path = require('path')
 const fs = require('fs-extra')
+const models = require('./models')
 
 app.engine('mustache', mustache())
 app.set('view engine', 'mustache')
@@ -15,26 +16,70 @@ app.listen(3000, function(){
   console.log("GOOD TO GO!!");
 })
 
-
-const todos = ["Wash the car"]
-for (var i = 0; i < todos.length; i++) {
-  var task = todos[i]
-}
-const dones = ["Fix the sink"]
-for (var i = 0; i < dones.length; i++) {
-  var done = dones[i]
-}
-
-app.get('/index', function(req, res){
-
-  res.render('index', {todos: todos,
-  task: task,
-  done: done})
+app.get('/', function(req, res){
+  models.Task.findAll({where:{completed:false}})
+    .then(function(tasks2){
+      var incompleteTasks = tasks2
+      models.Task.findAll({where:{completed:true}})
+      .then(function(tasks3){
+        var completedTasks = tasks3
+        res.render('index', {tasks: incompleteTasks,
+                completedTasks: completedTasks})
+      })
+  })
 })
+app.post('/complete', function (req, res){
+    models.Task.findOne({
+        where:{name: req.body.done}
+            }).then( function(task){
+                task.update({completed: true})
+                    }).then( function(){
+                        res.redirect('/')
+        })
 
-app.post('/index', function(req, res){
-  todos.push(req.body.todo)
-  dones.push(req.body.task)
+});
+app.post('/delete', function (req, res){
+    models.Task.findAll({
+        where: {completed: true}
+    }).then( function(tasks){
+        for (var i = 0; i < tasks.length; i++){
+            tasks[i].destroy()
+        }
+    })
+    res.redirect('/')
+});
 
-  res.redirect('/index')
-})
+app.post('/', function(req, res){
+    var task = models.Task.build({
+        name: req.body.task,
+        completed: false
+    });
+    task.save()
+    res.redirect('/')
+
+});
+// app.post('/', function(req, res){
+//     var task = models.Task.build({
+//       name: req.body.task,
+//       completed: false
+//     })
+// })
+//
+// app.post('/complete', function(req, res){
+//     models.Task.findOne({
+//       where: {name: req.body.done}
+//     }).then( function(task){
+//       task.update({completed:true})
+//     }).then(function(){
+//       res.redirect('/')
+// })
+//
+// app.post('/delete', function(req, res){
+//   models.Task.findAll({where:{compelted:true}})
+//     .then(function(tasks){
+//       for (var i = 0; i < tasks.length; i++) {
+//         tasks[i].destroy().then(function(){
+//             res.redirect('/')
+//           })
+//         }
+// })
